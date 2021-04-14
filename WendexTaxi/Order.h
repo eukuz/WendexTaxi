d6fd29b6381740d0e38ece0f4a2f4f4df722a130
eukuz <evgn.worker@gmail.com>
2021-04-14 19:47:53 +0300

    Order class added

---
+++ WendexTaxi/Order.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+using namespace std;
+class Order
+{
+public:
+	int ID;
+	int PassengerID;
+	int DriverID;
+	int CarTypeID;
+	int FromX;
+	int ToX;
+	int UnixTime;
+};
+

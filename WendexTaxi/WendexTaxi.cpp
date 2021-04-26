698cfe5872ae360e8f28ee95575a16a8fbc1c087
eukuz <evgn.worker@gmail.com>
2021-04-26 20:04:12 +0300

    Fixed Driver

--- WendexTaxi/WendexTaxi.cpp
+++ WendexTaxi/WendexTaxi.cpp
@@ -18,58 +18,57 @@ int main()
     dg->Login("Dave"); //Login in the system as a Driver
     pg->Login("Peter");//Login in the system as a Passenger
     ag->Login("Amelie"); //Login in the system as a Passenger
-    
-    /*
-    pg->PrintOrderHistory();
+ 
+    //pg->PrintOrderHistory();
 
-    pg->CheckRide(0, 10, Business);  // Checking a ride
-    pg->CheckRide(10, 2, Economy);
+    //pg->CheckRide(0, 10, Business);  // Checking a ride
+    //pg->CheckRide(10, 2, Economy);
 
-    Driver* d = DBShell::findDriver(Economy);
-    int from = 1, to = 7;
+    //Driver* d = DBShell::findDriver(Economy);
+    //int from = 1, to = 7;
 
-    pg->OrderRide(from, to, Economy, 1); // ordering a ride
+    //pg->OrderRide(from, to, Economy, 1); // ordering a ride
 
-    vector<Order*> av = dg->GetOrdersAvailable();
+    //vector<Order*> av = dg->GetOrdersAvailable();
 
-    dg->driver = d;
-    dg->StartRide(av.at(0)); //accepting a ride by a driver
+    //dg->driver = d;
+    //dg->StartRide(av.at(0)); //accepting a ride by a driver
 
-    for (int i = from; i <= to; i++) //the actual ride
-    {
-        dg->Move(i);
-        pg->WhereAmI(); //Get passenger's coordiantes during the ride
-    }
-    dg->FinishRide(av.at(0));
+    //for (int i = from; i <= to; i++) //the actual ride
+    //{
+    //    dg->Move(i);
+    //    pg->WhereAmI(); //Get passenger's coordiantes during the ride
+    //}
+    //dg->FinishRide(av.at(0));
 
-    dg->PrintOrderHistory(); //The ride appears in the order history when finished
-    
+    //dg->PrintOrderHistory(); //The ride appears in the order history when finished
+    //
 
-    pg->PrintOrderHistory(); //get the order history of a passanger and a bill
-    int bill = pg->GetBill(2);
+    //pg->PrintOrderHistory(); //get the order history of a passanger and a bill
+    //int bill = pg->GetBill(2);
 
-    vector<PaymentMethod*> pays = pg->GetListOfMethodsAvailable(); //get & update the list of payment methods
-    pg->UpdateMyListOfPaymentMethods(vector<PaymentMethod*>(&pays[0], &pays[2]));
-
-
-    Car* car = dg->GetMyCar(); //get a car of a Driver
-    
-    dg->SetStatus(NotWorking); //Set a status for a driver
+    //vector<PaymentMethod*> pays = pg->GetListOfMethodsAvailable(); //get & update the list of payment methods
+    //pg->UpdateMyListOfPaymentMethods(vector<PaymentMethod*>(&pays[0], &pays[2]));
 
 
+    //Car* car = dg->GetMyCar(); //get a car of a Driver
+    //
+    //dg->SetStatus(NotWorking); //Set a status for a driver
 
-    vector <int> adrs = pg->GetListOfPinnedAdresses(); //Get Update Pinned Addresses of a user
-    adrs.push_back(13);
-    adrs.push_back(28);
-    pg->UpdateMyListOfPinnedAddresses(adrs);
 
-    Car cBusiness = Car(-1, "a", Business, 1, "black", "12d"); //Park in front of the enrance check
-    Car cComfort = Car(-2, "b", Comfort, 1, "black", "12d");
 
-    cBusiness.ParkInFrontOfTheEntrance();
-    cComfort.ParkInFrontOfTheEntrance();
+    //vector <int> adrs = pg->GetListOfPinnedAdresses(); //Get Update Pinned Addresses of a user
+    //adrs.push_back(13);
+    //adrs.push_back(28);
+    //pg->UpdateMyListOfPinnedAddresses(adrs);
 
-    */
+    //Car cBusiness = Car(-1, "a", Business, 1, "black", "12d"); //Park in front of the enrance check
+    //Car cComfort = Car(-2, "b", Comfort, 1, "black", "12d");
+
+    //cBusiness.ParkInFrontOfTheEntrance();
+    //cComfort.ParkInFrontOfTheEntrance();
+
+    //
 
 
     cout << "Finished!";
